8920a3f1dc699edbbc42966a76859a867676d102
kryptohaker <kryptohaker@hackersnotes.com>
2021-08-07 23:15:15 +0200

    Updated Tool

--- xbg/xbg.py
+++ xbg/xbg.py
@@ -1,4 +1,17 @@
+import argparse
 import socket
-bangrab = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-bangrab.connect (("TARGET_IP", TARGET_PORT))
-print (bangrab.recv(4096))
+
+parser = argparse.ArgumentParser(description='Use this tool to grab the banner from the specified target.')
+parser.add_argument('t', metavar='IP', type=str, nargs='+',
+                    help='TARGET IP')
+parser.add_argument('p', metavar='PORT', type=int, nargs='+',
+                    help='TARGET PORT')
+
+args = parser.parse_args()
+if args.t!=None and args.p!=None:
+    bangrab = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    bangrab.connect ((args.t[0], args.p[0]))
+    print (bangrab.recv(4096))
+    
+else:
+    print("-h, --help  show this help message and exit")
